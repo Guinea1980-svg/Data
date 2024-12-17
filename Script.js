e79932ad5e8f87a517c4b35f9fd93d7bfe80ad54
GuineOnBlitz <24HaleEl@berkeleygreenutc.org.uk>
2024-12-17 11:29:08 +0000

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,17 @@
 
+function Redirect(Link) {
+	window.location.href = Link
+}
+function download(file, text) {
+
+        //creating an invisible element
+        let element = document.createElement('a');
+        element.setAttribute('href',
+            'data:text/plain;charset=utf-8, '
+            + encodeURIComponent(text));
+        element.setAttribute('download', file);
+        document.body.appendChild(element);
+        element.click();
+
+	document.body.removeChild(element);
+}
